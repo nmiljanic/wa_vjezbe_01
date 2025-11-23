9f84de8c7a56ec71bfd0902f999f3bff7a75a126
Niko Miljanić <nmiljanic@unipu.hr>
2025-11-23 20:21:53 +0100

    Samostalni zadatak za vježbu 1

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/users', (req, res) => {
+
+    const users = [
+        { id: 1, ime: "Niko", prezime: "Miljanić" },
+        { id: 2, ime: "Ema", prezime: "Sinković" },
+        { id: 3, ime: "Lucija", prezime: "Špada" }
+    ];
+
+    res.json(users)
+})
 
 app.listen(PORT, error => {
     if (error) {
@@ -14,3 +27,4 @@ app.listen(PORT, error => {
         console.log(`Server je pokrenut na http://localhost:${PORT}`);
     }
 });
+
